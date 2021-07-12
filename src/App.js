a03ab4c1eed75cfa4375b3407e12dff34ce4f334
kscheltema <k87scheltema@outlook.com>
2021-07-12 13:08:04 +0200

    transition animation simple  example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { Transition } from "react-transition-group";
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
@@ -7,24 +7,57 @@ import List from "./components/List/List";
 
 class App extends Component {
   state = {
-    modalIsOpen: false
-  }
-
-  showModal = () => {
-    this.setState({modalIsOpen:true});
+    modalIsOpen: false,
+    showBlock: false,
   };
 
-  closedModal = () =>  {
-    this.setState({modalIsOpen:false});
+  showModal = () => {
+    this.setState({ modalIsOpen: true });
+  };
+
+  closedModal = () => {
+    this.setState({ modalIsOpen: false });
   };
 
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closedModal}/>
-        <Backdrop show={this.state.modalIsOpen} />
-        <button className="Button" onClick={this.showModal}>Open Modal</button>
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+        <Transition in={this.state.showBlock} timeout={400}>
+          {(state) => <p>{state}</p>}
+          {/* <div
+            style={{
+              backgroundColor: "#f6f",
+              width: 100,
+              height: 100,
+              margin: "auto",
+            }}
+          /> */}
+        </Transition>
+        {/* {this.state.showBlock ? (
+          <div
+            style={{
+              backgroundColor: "#f6f",
+              width: 100,
+              height: 100,
+              margin: "auto",
+            }}
+          />
+        ) : null} */}
+        {this.state.modalIsOpen ? <Modal closed={this.closedModal} /> : null}
+        {this.state.modalIsOpen ? <Backdrop /> : null}
+        <button className="Button" onClick={this.showModal}>
+          Open Modal
+        </button>
         <h3>Animating Lists</h3>
         <List />
       </div>
